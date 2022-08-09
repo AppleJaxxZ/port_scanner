aa49b156fa4f754bc8c1f695b0794b78a6442ad0
Alex Zajacek <smackanalex@gmail.com>
2022-08-09 12:00:07 -0400

    created main script and main file

---
+++ main.py
@@ -0,0 +1,29 @@
+from tkinter import *
+from script import *
+
+
+class MyWindow:
+    def __init__(self, win):
+        self.lbl1 = Label(text="Enter the targets Ip Address",
+                          fg='red', font=("Helvetica", 16))
+        self.lbl1.place(x=52, y=120)
+        self.txtfld1 = Entry(text="This is Entry Widget", bd=5)
+        self.txtfld1.place(x=50, y=150)
+        self.btn1 = Button(win, text="Run Nmap Scan",
+                           fg='blue', command=self.scan)
+        self.btn1.place(x=80, y=190)
+
+    def scan(self):
+        address = str(self.txtfld1.get())
+        print(address)
+        port_scanner(address)
+
+
+window = Tk()
+mywin = MyWindow(window)
+window.title('Nmap')
+window.geometry("300x300")
+# Code
+
+
+window.mainloop()
